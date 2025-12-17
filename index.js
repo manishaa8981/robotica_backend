9fefc38784785e066e7e6356df4eabf1ac032fd7
manishaa8981 <manishaa8981@gmail.com>
2025-12-17 12:25:34 +0545

    origin

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-const corsPolicy = [
+const allowedOrigins = [
   "https://roboticainstitute.com",
   "https://www.roboticainstitute.com",
   "https://admin.roboticainstitute.com",
